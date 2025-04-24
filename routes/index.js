21cd62f1d5d036965dbb74028bd80b2fe0272fbb
mombeh nadine <nadinemombeh@gmail.com>
2025-04-24 14:05:01 +0100

    created a user login

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 import express from 'express';
 const router = express.Router();
 
+import { connectToDb } from "./config/db.js";
+import { initializeDbSchema } from "./config/schema.js";
+
+await connectToDb();
+await initializeDbSchema();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
